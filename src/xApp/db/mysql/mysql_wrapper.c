1d6bec47b127b7ba6792f7f4c866311df732b9cc
Chieh-Chun Chen <chenc80348@gmail.com>
2023-02-02 11:29:17 +0100

    Add rlc stat in mysql db and Fix write_mac_stat()

--- src/xApp/db/mysql/mysql_wrapper.c
+++ src/xApp/db/mysql/mysql_wrapper.c
@@ -99,6 +99,62 @@ void create_mac_ue_table(MYSQL* conn)
 
 }
 
+static
+void create_rlc_bearer_table(MYSQL* conn)
+{
+  assert(conn != NULL);
+
+  // ToDo: PRIMARY KEY UNIQUE
+  if(mysql_query(conn, "DROP TABLE IF EXISTS RLC_bearer"))
+    mysql_finish_with_error(conn);
+  char* sql_rlc = "CREATE TABLE RLC_bearer("
+                  "tstamp BIGINT CHECK(tstamp > 0),"
+                  "ngran_node INT CHECK(ngran_node >= 0 AND ngran_node < 9),"
+                  "mcc INT,"
+                  "mnc INT,"
+                  "mnc_digit_len INT,"
+                  "nb_id INT,"
+                  "cu_du_id TEXT,"
+                  "txpdu_pkts BIGINT CHECK(txpdu_pkts >= 0 AND txpdu_pkts < 4294967296)," // 1 << 32 = 4294967296
+                  "txpdu_bytes BIGINT CHECK(txpdu_bytes >= 0 AND txpdu_bytes < 4294967296),"
+                  "txpdu_wt_ms BIGINT CHECK(txpdu_wt_ms >= 0 AND txpdu_wt_ms < 4294967296),"
+                  "txpdu_dd_pkts BIGINT CHECK(txpdu_dd_pkts >= 0 AND txpdu_dd_pkts < 4294967296),"
+                  "txpdu_dd_bytes BIGINT CHECK(txpdu_dd_bytes >= 0 AND txpdu_dd_bytes < 4294967296),"
+                  "txpdu_retx_pkts BIGINT CHECK(txpdu_retx_pkts >= 0 AND txpdu_retx_pkts < 4294967296),"
+                  "txpdu_retx_bytes BIGINT CHECK(txpdu_retx_bytes >= 0 AND txpdu_retx_bytes < 4294967296),"
+                  "txpdu_segmented BIGINT CHECK(txpdu_segmented >= 0 AND txpdu_segmented < 4294967296),"
+                  "txpdu_status_pkts BIGINT CHECK(txpdu_status_pkts >= 0 AND txpdu_status_pkts < 4294967296),"
+                  "txpdu_status_bytes BIGINT CHECK(txpdu_status_bytes >= 0 AND txpdu_status_bytes < 4294967296),"
+                  "txbuf_occ_bytes BIGINT CHECK(txbuf_occ_bytes >= 0 AND txbuf_occ_bytes < 4294967296),"
+                  "txbuf_occ_pkts BIGINT CHECK(txbuf_occ_pkts >= 0 AND txbuf_occ_pkts < 4294967296),"
+                  "rxpdu_pkts BIGINT CHECK(rxpdu_pkts >= 0 AND rxpdu_pkts < 4294967296),"
+                  "rxpdu_bytes BIGINT CHECK(rxpdu_bytes >= 0 AND rxpdu_bytes < 4294967296),"
+                  "rxpdu_dup_pkts BIGINT CHECK(rxpdu_dup_pkts >= 0 AND rxpdu_dup_pkts < 4294967296),"
+                  "rxpdu_dup_bytes BIGINT CHECK(rxpdu_dup_bytes >= 0 AND rxpdu_dup_bytes < 4294967296),"
+                  "rxpdu_dd_pkts BIGINT CHECK(rxpdu_dd_pkts >= 0 AND rxpdu_dd_pkts < 4294967296),"
+                  "rxpdu_dd_bytes BIGINT CHECK(rxpdu_dd_bytes >= 0 AND rxpdu_dd_bytes < 4294967296),"
+                  "rxpdu_ow_pkts BIGINT CHECK(rxpdu_ow_pkts >= 0 AND rxpdu_ow_pkts < 4294967296),"
+                  "rxpdu_ow_bytes BIGINT CHECK(rxpdu_ow_bytes >= 0 AND rxpdu_ow_bytes < 4294967296),"
+                  "rxpdu_status_pkts BIGINT CHECK(rxpdu_status_pkts >= 0 AND rxpdu_status_pkts < 4294967296),"
+                  "rxpdu_status_bytes BIGINT CHECK(rxpdu_status_bytes >= 0 AND rxpdu_status_bytes < 4294967296),"
+                  "rxbuf_occ_bytes BIGINT CHECK(rxbuf_occ_bytes >= 0 AND rxbuf_occ_bytes < 4294967296),"
+                  "rxbuf_occ_pkts BIGINT CHECK(rxbuf_occ_pkts >= 0 AND rxbuf_occ_pkts < 4294967296),"
+                  "txsdu_pkts BIGINT CHECK(txsdu_pkts >= 0 AND txsdu_pkts < 4294967296),"
+                  "txsdu_bytes BIGINT CHECK(txsdu_bytes >= 0 AND txsdu_bytes < 4294967296),"
+                  "rxsdu_pkts BIGINT CHECK(rxsdu_pkts >= 0 AND rxsdu_pkts < 4294967296),"
+                  "rxsdu_bytes BIGINT CHECK(rxsdu_bytes >= 0 AND rxsdu_bytes < 4294967296),"
+                  "rxsdu_dd_pkts BIGINT CHECK(rxsdu_dd_pkts >= 0 AND rxsdu_dd_pkts < 4294967296),"
+                  "rxsdu_dd_bytes BIGINT CHECK(rxsdu_dd_bytes >= 0 AND rxsdu_dd_bytes < 4294967296),"
+                  "rnti BIGINT CHECK(rnti >= 0 AND rnti < 4294967296),"
+                  "mode INT CHECK(mode >= 0 AND mode < 3),"
+                  "rbid INT CHECK(rbid >= 0 AND rbid < 16)"
+                  ")";
+
+  if(mysql_query(conn, sql_rlc))
+    mysql_finish_with_error(conn);
+
+}
+
 //static
 //void insert_db(sqlite3* db, char const* sql)
 //{
@@ -276,21 +332,254 @@ int to_mysql_string_mac_ue(global_e2_node_id_t const* id, mac_ue_stats_impl_t* s
 }
 
 static
+int to_mysql_string_rlc_rb(global_e2_node_id_t const* id,rlc_radio_bearer_stats_t* rlc, int64_t tstamp, char* out, size_t out_len)
+{
+  assert(rlc != NULL);
+  assert(out != NULL);
+  const size_t max = 1024;
+  assert(out_len >= max);
+
+  char* c_null = NULL;
+  char c_cu_du_id[26];
+  if (id->cu_du_id) {
+    int rc = snprintf(c_cu_du_id, 26, "%lu", *id->cu_du_id);
+    assert(rc < (int) max && "Not enough space in the char array to write all the data");
+  }
+
+  int const rc = snprintf(out, out_len,
+                                    "("
+                                    "%ld,"// tstamp
+                                    "%d," //ngran_node
+                                    "%d," //mcc
+                                    "%d," //mnc
+                                    "%d," //mnc_digit_len
+                                    "%d," //nb_id
+                                    "'%s'," //cu_du_id
+                                    "%u," //rlc->txpdu_pkts
+                                    "%u," //rlc->txpdu_bytes
+                                    "%u," //rlc->txpdu_wt_ms
+                                    "%u," //rlc->txpdu_dd_pkts
+                                    "%u," //rlc->txpdu_dd_bytes
+                                    "%u," //rlc->txpdu_retx_pkts
+                                    "%u," //rlc->txpdu_retx_bytes
+                                    "%u," //rlc->txpdu_segmented
+                                    "%u," //rlc->txpdu_status_pkts
+                                    "%u," //rlc->txpdu_status_bytes
+                                    "%u," //rlc->txbuf_occ_bytes
+                                    "%u," //rlc->txbuf_occ_pkts
+                                    "%u," //rlc->rxpdu_pkts
+                                    "%u," //rlc->rxpdu_bytes
+                                    "%u," //rlc->rxpdu_dup_pkts
+                                    "%u," //rlc->rxpdu_dup_bytes
+                                    "%u," //rlc->rxpdu_dd_pkts
+                                    "%u," //rlc->rxpdu_dd_bytes
+                                    "%u," //rlc->rxpdu_ow_pkts
+                                    "%u," //rlc->rxpdu_ow_bytes
+                                    "%u," //rlc->rxpdu_status_pkts
+                                    "%u," //rlc->rxpdu_status_bytes
+                                    "%u," //rlc->rxbuf_occ_bytes
+                                    "%u," //rlc->rxbuf_occ_pkts
+                                    "%u," //rlc->txsdu_pkts
+                                    "%u," //rlc->txsdu_bytes
+                                    "%u," //rlc->rxsdu_pkts
+                                    "%u," //rlc->rxsdu_bytes
+                                    "%u," //rlc->rxsdu_dd_pkts
+                                    "%u," //rlc->rxsdu_dd_bytes
+                                    "%u," //rlc->rnti
+                                    "%u,"  //rlc->mode
+                                    "%u"  //rlc->rbid
+                                    ")",
+                                    tstamp,
+                                    id->type,
+                                    id->plmn.mcc,
+                                    id->plmn.mnc,
+                                    id->plmn.mnc_digit_len,
+                                    id->nb_id,
+                                    id->cu_du_id ? c_cu_du_id : c_null,
+                                    rlc->txpdu_pkts,
+                                    rlc->txpdu_bytes,
+                                    rlc->txpdu_wt_ms,
+                                    rlc->txpdu_dd_pkts,
+                                    rlc->txpdu_dd_bytes,
+                                    rlc->txpdu_retx_pkts,
+                                    rlc->txpdu_retx_bytes,
+                                    rlc->txpdu_segmented,
+                                    rlc->txpdu_status_pkts,
+                                    rlc->txpdu_status_bytes,
+                                    rlc->txbuf_occ_bytes,
+                                    rlc->txbuf_occ_pkts,
+                                    rlc->rxpdu_pkts,
+                                    rlc->rxpdu_bytes,
+                                    rlc->rxpdu_dup_pkts,
+                                    rlc->rxpdu_dup_bytes,
+                                    rlc->rxpdu_dd_pkts,
+                                    rlc->rxpdu_dd_bytes,
+                                    rlc->rxpdu_ow_pkts,
+                                    rlc->rxpdu_ow_bytes,
+                                    rlc->rxpdu_status_pkts,
+                                    rlc->rxpdu_status_bytes,
+                                    rlc->rxbuf_occ_bytes,
+                                    rlc->rxbuf_occ_pkts,
+                                    rlc->txsdu_pkts,
+                                    rlc->txsdu_bytes,
+                                    rlc->rxsdu_pkts,
+                                    rlc->rxsdu_bytes,
+                                    rlc->rxsdu_dd_pkts,
+                                    rlc->rxsdu_dd_bytes,
+                                    rlc->rnti,
+                                    rlc->mode,
+                                    rlc->rbid);
+
+  assert(rc < (int)max && "Not enough space in the char array to write all the data");
+  return rc;
+}
+
+static
 void write_mac_stats(MYSQL* conn, global_e2_node_id_t const* id, mac_ind_data_t const* ind) {
   assert(conn != NULL);
   assert(ind != NULL);
 
   mac_ind_msg_t const *ind_msg_mac = &ind->msg;
 
-  for (size_t i = 0; i < ind_msg_mac->len_ue_stats; ++i) {
-    char buffer[2048] = {0};
-    int pos = 0;
-    pos += to_mysql_string_mac_ue(id, &ind_msg_mac->ue_stats[i], ind_msg_mac->tstamp, buffer + pos, 2048 - pos);
-    if (mysql_query(conn, buffer))
-      mysql_finish_with_error(conn);
-    //printf("MySQL Insert data Successful\n");
-  }
+  char buffer[2048] = "INSERT INTO MAC_UE "
+                      "("
+                      "tstamp,"
+                      "ngran_node,"
+                      "mcc,"
+                      "mnc,"
+                      "mnc_digit_len,"
+                      "nb_id,"
+                      "cu_du_id,"
+                      "frame,"
+                      "slot,"
+                      "dl_aggr_tbs,"
+                      "ul_aggr_tbs,"
+                      "dl_aggr_bytes_sdus,"
+                      "ul_aggr_bytes_sdus,"
+                      "dl_curr_tbs,"
+                      "ul_curr_tbs,"
+                      "dl_sched_rb,"
+                      "ul_sched_rb,"
+                      "pusch_snr,"
+                      "pucch_snr,"
+                      "rnti,"
+                      "dl_aggr_prb,"
+                      "ul_aggr_prb,"
+                      "dl_aggr_sdus,"
+                      "ul_aggr_sdus,"
+                      "dl_aggr_retx_prb,"
+                      "ul_aggr_retx_prb,"
+                      "wb_cqi,"
+                      "dl_mcs1,"
+                      "ul_mcs1,"
+                      "dl_mcs2,"
+                      "ul_mcs2,"
+                      "phr,"
+                      "bsr,"
+                      "dl_bler,"
+                      "ul_bler,"
+                      "dl_num_harq,"
+                      "dl_harq_round0,"
+                      "dl_harq_round1,"
+                      "dl_harq_round2,"
+                      "dl_harq_round3,"
+                      "dlsch_errors,"
+                      "ul_num_harq,"
+                      "ul_harq_round0,"
+                      "ul_harq_round1,"
+                      "ul_harq_round2,"
+                      "ul_harq_round3,"
+                      "ulsch_errors"
+                      ") "
+                      "VALUES";
+  int pos = strlen(buffer);
 
+  for (size_t i = 0; i < ind_msg_mac->len_ue_stats; ++i) {
+    pos += to_mysql_string_mac_ue(id, &ind_msg_mac->ue_stats[i], ind_msg_mac->tstamp, buffer + pos, 2048 - pos);
+    if (i + 1 < ind_msg_mac->len_ue_stats) {
+      strncat(buffer, ",", 1);
+      pos +=1;
+    }
+  }
+  strncat(buffer, ";", 1);
+
+  for(int i = 0; i < strlen(buffer); i++)
+    printf("%c", buffer[i]);
+  printf("\n");
+  if (mysql_query(conn, buffer))
+    mysql_finish_with_error(conn);
+  //printf("[MySQL]: Insert MAC data Successful\n");
+}
+
+static
+void write_rlc_stats(MYSQL* conn, global_e2_node_id_t const* id, rlc_ind_data_t const* ind)
+{
+  assert(conn != NULL);
+  assert(ind != NULL);
+
+  rlc_ind_msg_t const* ind_msg_rlc = &ind->msg;
+
+  char buffer[2048] = "INSERT INTO RLC_bearer "
+                      "("
+                      "tstamp,"
+                      "ngran_node,"
+                      "mcc,"
+                      "mnc,"
+                      "mnc_digit_len,"
+                      "nb_id,"
+                      "cu_du_id,"
+                      "txpdu_pkts,"
+                      "txpdu_bytes,"
+                      "txpdu_wt_ms,"
+                      "txpdu_dd_pkts,"
+                      "txpdu_dd_bytes,"
+                      "txpdu_retx_pkts,"
+                      "txpdu_retx_bytes,"
+                      "txpdu_segmented,"
+                      "txpdu_status_pkts,"
+                      "txpdu_status_bytes,"
+                      "txbuf_occ_bytes,"
+                      "txbuf_occ_pkts,"
+                      "rxpdu_pkts,"
+                      "rxpdu_bytes,"
+                      "rxpdu_dup_pkts,"
+                      "rxpdu_dup_bytes,"
+                      "rxpdu_dd_pkts,"
+                      "rxpdu_dd_bytes,"
+                      "rxpdu_ow_pkts,"
+                      "rxpdu_ow_bytes,"
+                      "rxpdu_status_pkts,"
+                      "rxpdu_status_bytes,"
+                      "rxbuf_occ_bytes,"
+                      "rxbuf_occ_pkts,"
+                      "txsdu_pkts,"
+                      "txsdu_bytes,"
+                      "rxsdu_pkts,"
+                      "rxsdu_bytes,"
+                      "rxsdu_dd_pkts,"
+                      "rxsdu_dd_bytes,"
+                      "rnti,"
+                      "mode,"
+                      "rbid"
+                      ") "
+                      "VALUES";
+  int pos = strlen(buffer);
+
+  for(size_t i = 0; i < ind_msg_rlc->len; ++i){
+    pos += to_mysql_string_rlc_rb(id, &ind_msg_rlc->rb[i], ind_msg_rlc->tstamp, buffer + pos, 2048 - pos);
+    if (i + 1 < ind_msg_rlc->len) {
+      strncat(buffer, ",", 1);
+      pos +=1;
+    }
+  }
+  strncat(buffer, ";", 1);
+
+  for(int i = 0; i < strlen(buffer); i++)
+    printf("%c", buffer[i]);
+  printf("\n");
+  if (mysql_query(conn, buffer))
+    mysql_finish_with_error(conn);
+  printf("[MySQL]: Insert RLC data Successful\n");
 }
 
 void init_db_mysql(MYSQL* conn, char const* db_filename)
@@ -311,6 +600,14 @@ void init_db_mysql(MYSQL* conn, char const* db_filename)
   //////
   mysql_query(conn, "USE testdb");
   create_mac_ue_table(conn);
+  printf("[MySQL]: Create New MAC_UE Table Successful\n");
+
+  //////
+  // RLC
+  //////
+  mysql_query(conn, "USE testdb");
+  create_rlc_bearer_table(conn);
+  printf("[MySQL]: Create New RLC_bearer Table Successful\n");
 
 }
 
@@ -325,12 +622,13 @@ void write_db_mysql(MYSQL* conn, global_e2_node_id_t const* id, sm_ag_if_rd_t co
 {
   assert(conn != NULL);
   assert(rd != NULL);
-  assert(rd->type == MAC_STATS_V0);
+  assert(rd->type == MAC_STATS_V0 || rd->type == RLC_STATS_V0);
 
   if(rd->type == MAC_STATS_V0){
     write_mac_stats(conn, id, &rd->mac_stats);
-//  } else if(rd->type == RLC_STATS_V0 ){
-//    write_rlc_stats(db, id, &rd->rlc_stats);
+  } else if(rd->type == RLC_STATS_V0 ){
+    printf("[MYSQL]: Try to write rlc data in mysql db\n");
+    write_rlc_stats(conn, id, &rd->rlc_stats);
 //  } else if( rd->type == PDCP_STATS_V0) {
 //    write_pdcp_stats(db, id, &rd->pdcp_stats);
 //  } else if (rd->type == SLICE_STATS_V0) {
