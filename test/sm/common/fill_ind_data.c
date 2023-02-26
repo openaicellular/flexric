7e02530c3c53a50cf5a2887408897c55b12f6148
Chieh-Chun Chen <chenc80348@gmail.com>
2023-02-26 08:49:39 +0100

    Revert "wrong conversion for RNTI"
    This reverts commit b112415d9891ab11fe597e73903610e34e30edbb.

--- test/sm/common/fill_ind_data.c
+++ test/sm/common/fill_ind_data.c
@@ -148,13 +148,8 @@ void fill_kpm_ind_data(kpm_ind_data_t* ind)
       adapter_MeasRecord_t* KPMRecord = calloc(KPMData[i].measRecord_len, sizeof(adapter_MeasRecord_t));
       KPMData[i].measRecord = KPMRecord;
       for (size_t j = 0; j < num_data; j++) {
-        if (j == 9){ // RNTI is integer
-          KPMRecord[j].type = MeasRecord_int;
-          KPMRecord[j].int_val = j == 0 ? t : abs(rand()%1024);
-        }else{
-          KPMRecord[j].type = MeasRecord_real;
-          KPMRecord[j].real_val = j == 0 ? t : abs(rand()%1024);
-        }
+        KPMRecord[j].type = MeasRecord_real;
+        KPMRecord[j].real_val = j == 0 ? t : abs(rand()%1024);
         // printf("fill kpm real value %f\n", KPMRecord[j].real_val);
       }
       KPMData[i].incompleteFlag = -1;
