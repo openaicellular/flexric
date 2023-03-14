aaad801489eafec0d014ea5edfe8d67d2caa1bcc
nguyenk <khai.nguyen@eurecom.fr>
2023-03-14 16:50:39 +0100

    fix: add condition monolithic node in helloword.go

--- examples/xApp/go/helloworld/helloworld.go
+++ examples/xApp/go/helloworld/helloworld.go
@@ -7,6 +7,30 @@ import (
 	"time"
 )
 
+func RanNametoInt(value string) int {
+	switch value {
+	case "ngran_eNB":
+		return 0
+	case "ngran_ng_eNB":
+		return 1
+	case "ngran_gNB":
+		return 2
+	case "ngran_eNB_CU":
+		return 3
+	case "ngran_ng_eNB_CU":
+		return 4
+	case "ngran_gNB_CU":
+		return 5
+	case "ngran_eNB_DU":
+		return 6
+	case "ngran_gNB_DU":
+		return 7
+	case "ngran_eNB_MBMS_STA":
+		return 8
+	}
+	return -1
+}
+
 // ------------------------------------------------------------------------ //
 //
 //	MAIN
@@ -29,16 +53,34 @@ func main() {
 
 	var i int64 = 0
 	for ; i <= nodes_len-1; i++ {
-		var ranType xapp.E2Node = conn.Get(int(i))
+		var e2Node xapp.E2Node = conn.Get(int(i))
 		// TODO: Add condition if node is monolothic
-		fmt.Printf("E2 node %d info: nb_id %d, mcc %d, mnc %d, mnc_digit_len %d\n",
-			i,
-			ranType.GetId().GetNb_id(),
-			ranType.GetId().GetPlmn().GetMcc(),
-			ranType.GetId().GetPlmn().GetMnc(),
-			ranType.GetId().GetPlmn().GetMnc_digit_len(),
-			// TODO: get RAN type
-		)
+		var ranTypeName string = xapp.Get_ngran_name(e2Node.GetId().GetXtype())
+		// if Node is monolithic
+		if xapp.Ngran_eNB == RanNametoInt(ranTypeName) ||
+			xapp.Ngran_ng_eNB == RanNametoInt(ranTypeName) ||
+			xapp.Ngran_gNB == RanNametoInt(ranTypeName) {
+			fmt.Printf("E2 node %d info: nb_id %d, mcc %d, mnc %d, mnc_digit_len %d, ran_type %s\n",
+				i,
+				e2Node.GetId().GetNb_id(),
+				e2Node.GetId().GetPlmn().GetMcc(),
+				e2Node.GetId().GetPlmn().GetMnc(),
+				e2Node.GetId().GetPlmn().GetMnc_digit_len(),
+				ranTypeName,
+			)
+		} else {
+			fmt.Printf("E2 node %d info: nb_id %d, mcc %d, mnc %d, mnc_digit_len %d, ran_type %s, cu_du_id %d\n",
+				i,
+				e2Node.GetId().GetNb_id(),
+				e2Node.GetId().GetPlmn().GetMcc(),
+				e2Node.GetId().GetPlmn().GetMnc(),
+				e2Node.GetId().GetPlmn().GetMnc_digit_len(),
+				ranTypeName,
+				*e2Node.GetId().GetCu_du_id(),
+			)
+		}
+		fmt.Printf("E2 node %d supported RAN function's IDs: \n", i)
+		// TODO: print all the RAN function
 	}
 
 	// ----------------------- END ----------------------- //
