8b5c7c06552850c8b72a3ea7ff713045ae7836c2
Chieh-Chun Chen <chenc80348@gmail.com>
2023-05-10 18:12:45 +0200

    Fix: call send_sub_req function for each sm

--- examples/xApp/python3/xapp_mac_rlc_pdcp_moni.py
+++ examples/xApp/python3/xapp_mac_rlc_pdcp_moni.py
@@ -2,12 +2,14 @@ import xapp_sdk as ric
 import time
 import os
 import pdb
+import csv
+
 
 ####################
 #### MAC INDICATION CALLBACK
 ####################
 
-#  MACCallback class is defined and derived from C++ class mac_cb
+# MACCallback class is defined and derived from C++ class mac_cb
 class MACCallback(ric.mac_cb):
     # Define Python class 'constructor'
     def __init__(self):
@@ -20,7 +22,7 @@ class MACCallback(ric.mac_cb):
             t_now = time.time_ns() / 1000.0
             t_mac = ind.tstamp / 1.0
             t_diff = t_now - t_mac
-            print(f"MAC Indication tstamp {t_now} diff {t_diff} e2 node type {ind.id.type} nb_id {ind.id.nb_id}")
+            print(f"MAC Indication tstamp {t_now} diff {t_diff} E2-node type {ind.id.type} nb_id {ind.id.nb_id}")
             # print('MAC rnti = ' + str(ind.ue_stats[0].rnti))
 
 ####################
@@ -38,8 +40,8 @@ class RLCCallback(ric.rlc_cb):
         if len(ind.rb_stats) > 0:
             t_now = time.time_ns() / 1000.0
             t_rlc = ind.tstamp / 1.0
-            t_diff = t_now - t_rlc
-            print(f"RLC Indication tstamp {t_now} diff {t_diff} e2 node type {ind.id.type} nb_id {ind.id.nb_id}")
+            t_diff= t_now - t_rlc
+            print(f"RLC Indication tstamp {t_now} diff {t_diff} E2-node type {ind.id.type} nb_id {ind.id.nb_id}")
             # print('RLC rnti = '+ str(ind.rb_stats[0].rnti))
 
 ####################
@@ -58,70 +60,68 @@ class PDCPCallback(ric.pdcp_cb):
             t_now = time.time_ns() / 1000.0
             t_pdcp = ind.tstamp / 1.0
             t_diff = t_now - t_pdcp
-            print(f"PDCP Indication tstamp {t_now} diff {t_diff} e2 node type {ind.id.type} nb_id {ind.id.nb_id}")
+            print(f"PDCP Indication tstamp {t_now} diff {t_diff} E2-node type {ind.id.type} nb_id {ind.id.nb_id}")
             # print('PDCP rnti = '+ str(ind.rb_stats[0].rnti))
 
+def send_sub_req(sm_name, id, cb):
+    global mac_hndlr
+    global rlc_hndlr
+    global pdcp_hndlr
+    if sm_name == "MAC":
+        hndlr = ric.report_mac_sm(id, ric.Interval_ms_1, cb)
+        mac_hndlr.append(hndlr)
+    elif sm_name == "RLC":
+        hndlr = ric.report_rlc_sm(id, ric.Interval_ms_1, cb)
+        rlc_hndlr.append(hndlr)
+    elif sm_name == "PDCP":
+        hndlr = ric.report_pdcp_sm(id, ric.Interval_ms_1, cb)
+        pdcp_hndlr.append(hndlr)
+    else:
+        print("Unknow SM")
 
 ####################
 ####  GENERAL 
 ####################
+if __name__ == '__main__':
 
-ric.init()
+    mac_hndlr = []
+    rlc_hndlr = []
+    pdcp_hndlr = []
+    # Start
+    ric.init()
+    conn = ric.conn_e2_nodes()
+    assert(len(conn) > 0)
+    for i in range(0, len(conn)):
+        print("Global E2 Node [" + str(i) + "]: PLMN MCC = " + str(conn[i].id.plmn.mcc))
+        print("Global E2 Node [" + str(i) + "]: PLMN MNC = " + str(conn[i].id.plmn.mnc))
 
-conn = ric.conn_e2_nodes()
-assert(len(conn) > 0)
-for i in range(0, len(conn)):
-    print("Global E2 Node [" + str(i) + "]: PLMN MCC = " + str(conn[i].id.plmn.mcc))
-    print("Global E2 Node [" + str(i) + "]: PLMN MNC = " + str(conn[i].id.plmn.mnc))
+    sm_name = "MAC"
+    for i in range(0, len(conn)):
+        mac_cb = MACCallback()
+        send_sub_req(sm_name, conn[i].id, mac_cb)
+    sm_name = "RLC"
+    for i in range(0, len(conn)):
+        rlc_cb = RLCCallback()
+        send_sub_req(sm_name, conn[i].id, rlc_cb)
+    sm_name = "PDCP"
+    for i in range(0, len(conn)):
+        pdcp_cb = PDCPCallback()
+        send_sub_req(sm_name, conn[i].id, pdcp_cb)
 
-####################
-#### MAC INDICATION
-####################
+    time.sleep(10)
 
-mac_hndlr = []
-for i in range(0, len(conn)):
-    mac_cb = MACCallback()
-    hndlr = ric.report_mac_sm(conn[i].id, ric.Interval_ms_1, mac_cb)
-    mac_hndlr.append(hndlr)     
-    time.sleep(1)
+    ### End
+    for i in range(0, len(mac_hndlr)):
+        ric.rm_report_mac_sm(mac_hndlr[i])
 
-####################
-#### RLC INDICATION
-####################
+    for i in range(0, len(rlc_hndlr)):
+        ric.rm_report_rlc_sm(rlc_hndlr[i])
 
-rlc_hndlr = []
-for i in range(0, len(conn)):
-    rlc_cb = RLCCallback()
-    hndlr = ric.report_rlc_sm(conn[i].id, ric.Interval_ms_1, rlc_cb)
-    rlc_hndlr.append(hndlr)
-    time.sleep(1)
+    for i in range(0, len(pdcp_hndlr)):
+        ric.rm_report_pdcp_sm(pdcp_hndlr[i])
 
-####################
-#### PDCP INDICATION
-####################
+    # Avoid deadlock. ToDo revise architecture
+    while ric.try_stop == 0:
+        time.sleep(1)
 
-pdcp_hndlr = []
-for i in range(0, len(conn)):
-    pdcp_cb = PDCPCallback()
-    hndlr = ric.report_pdcp_sm(conn[i].id, ric.Interval_ms_1, pdcp_cb)
-    pdcp_hndlr.append(hndlr)
-    time.sleep(1)
-
-time.sleep(10)
-
-### End
-
-for i in range(0, len(mac_hndlr)):
-    ric.rm_report_mac_sm(mac_hndlr[i])
-
-for i in range(0, len(rlc_hndlr)):
-    ric.rm_report_rlc_sm(rlc_hndlr[i])
-
-for i in range(0, len(pdcp_hndlr)):
-    ric.rm_report_pdcp_sm(pdcp_hndlr[i])
-
-# Avoid deadlock. ToDo revise architecture 
-while ric.try_stop == 0:
-    time.sleep(1)
-
-print("Test finished")
+    print("Test finished")
