b697b99233a8cdaae7321729959acdb89796b63d
Ilias Chatzistefanidis <Ilias.Chatzistefanidis@eurecom.fr>
2023-05-17 18:24:34 +0200

    add kpm python xApp printing latency

---
+++ examples/xApp/python3/xapp_kpm_moni.py
@@ -0,0 +1,70 @@
+import xapp_sdk as ric
+import time
+import os
+import pdb
+import time
+import signal
+import sys
+
+exit_flag = False
+
+####################
+#### KPM INDICATION CALLBACK
+####################
+
+# Create a callback for KPM which derived it from C++ class kpm_cb
+class KPMCallback(ric.kpm_cb):
+    def __init__(self):
+        # Inherit C++ kpm_cb class
+        ric.kpm_cb.__init__(self)
+    # Create an override C++ method 
+    def handle(self, ind):
+        now = int(time.time() * 1000000)
+        ts = 0
+        if len(ind.MeasData) > 0:
+            if len(ind.MeasData[0].measRecord) > 0:
+                ts = ind.MeasData[0].measRecord[0].real_val
+        
+        print(f"KPM ind_msg latency = {now - ts} us")
+
+
+####################
+####  GENERAL 
+####################
+
+ric.init()
+
+conn = ric.conn_e2_nodes()
+assert(len(conn) > 0)
+
+print("Connected E2 nodes =", len(conn))
+for i in range(0, len(conn)):
+    print("Global E2 Node [" + str(i) + "]: PLMN MCC = " + str(conn[i].id.plmn.mcc))
+    print("Global E2 Node [" + str(i) + "]: PLMN MNC = " + str(conn[i].id.plmn.mnc))
+
+
+####################
+#### KPM INDICATION
+####################
+
+kpm_hndlr = []
+
+for i in range(0, len(conn)):
+    kpm_cb = KPMCallback()
+    hndlr = ric.report_kpm_sm(conn[i].id, ric.Interval_ms_5, kpm_cb)
+    kpm_hndlr.append(hndlr)
+    time.sleep(1)
+
+
+time.sleep(10)
+
+### End
+
+for i in range(0, len(kpm_hndlr)):
+    ric.rm_report_kpm_sm(kpm_hndlr[i])
+
+# Avoid deadlock. ToDo revise architecture 
+while ric.try_stop == 0:
+    time.sleep(1)
+
+print("Test xApp run SUCCESSFULLY")
