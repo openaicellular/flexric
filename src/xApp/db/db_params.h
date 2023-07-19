b6f2a64cb014319bcb2fa8b60a98d59a32d194b4
Chieh-Chun Chen <chenc80348@gmail.com>
2023-07-19 11:13:57 +0200

    Add missing db_params.h

---
+++ src/xApp/db/db_params.h
@@ -0,0 +1,23 @@
+//
+// Created by modelx on 18/07/23.
+//
+
+#ifndef BR_FLEXRIC_DB_PARAMS_H
+#define BR_FLEXRIC_DB_PARAMS_H
+
+#include "util/conf_file.h"
+
+typedef struct{
+    bool enable;
+    char* ip;
+    char* dir;
+    char* db_name;
+#ifdef MYSQL_XAPP
+    char* user;
+    char* pass;
+#endif
+} db_params_t;
+
+void get_db_params(fr_args_t const* args, db_params_t* params);
+
+#endif //BR_FLEXRIC_DB_PARAMS_H
