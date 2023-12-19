5ea5d636fe2b3c601018f2a6c3b10570529ac37e
Chieh-Chun Chen <chenc80348@gmail.com>
2023-12-19 17:35:08 +0100

    Fix Unit_test_enc_dec_kpm_sm_v03

--- src/sm/kpm_sm/kpm_sm_v03.00/ie/kpm_data_ie/data/label_info_lst.c
+++ src/sm/kpm_sm/kpm_sm_v03.00/ie/kpm_data_ie/data/label_info_lst.c
@@ -140,11 +140,15 @@ bool eq_label_info(const label_info_lst_t *l1, const label_info_lst_t *l2)
   if (eq_e2sm_plmn(l1->plmn_id, l2->plmn_id) != true)
     return false;
 
-  assert(l1->sliceID == NULL && "Not implemented");
-  assert(l2->sliceID == NULL && "Not implemented");
+  if (eq_s_nssai_e2sm(l1->sliceID, l2->sliceID) != true)
+    return false;
 
-  assert(l1->fiveQI == NULL && "Not implemented");
-  assert(l2->fiveQI == NULL && "Not implemented");
+  if(l1->fiveQI != NULL || l2->fiveQI != NULL){
+    if(l1->fiveQI == NULL || l2->fiveQI == NULL)
+      return false;
+    if(*l1->fiveQI != *l2->fiveQI)
+      return false;
+  }
 
   assert(l1->qFI == NULL && "Not implemented");
   assert(l2->qFI == NULL && "Not implemented");
