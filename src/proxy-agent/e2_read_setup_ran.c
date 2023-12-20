4b0d8ea4fc3dd3bb1a7d341b831dc337f85d09a4
khai nguyen <khai.nguyen@bubbleran.com>
2023-12-20 15:36:52 +0100

    fix: proxy agent to support gNB only

--- src/proxy-agent/e2_read_setup_ran.c
+++ src/proxy-agent/e2_read_setup_ran.c
@@ -21,7 +21,6 @@
 
 #include "e2_read_setup_ran.h"
 #include "../src/lib/e2ap/e2ap_node_component_config_add_wrapper.h"
-#include "../src/util/e2ap_ngran_types.h"
 #include "../test/rnd/fill_rnd_data_e2_setup_req.h"
 #include <assert.h>
 #include <stdlib.h>
@@ -33,57 +32,12 @@ void read_setup_ran(void* data)
 
 #elif defined(E2AP_V2) || defined(E2AP_V3) 
   arr_node_component_config_add_t* dst = (arr_node_component_config_add_t*)data;
-  // TODO: Create a value assigned by CMakeLists.txt (check read_setup_ran.c)
-  e2ap_ngran_node_t const n = e2ap_ngran_gNB;
-  if(n == e2ap_ngran_gNB){
-    dst->len_cca = 1;
-    dst->cca = calloc(1, sizeof(e2ap_node_component_config_add_t));
-    assert(dst->cca != NULL);
-    // NGAP
-    dst->cca[0] = fill_ngap_e2ap_node_component_config_add();
-  } else if(n == e2ap_ngran_gNB_CU){
-    dst->len_cca = 2;
-    dst->cca = calloc(2, sizeof(e2ap_node_component_config_add_t));
-    assert(dst->cca != NULL);
-    // NGAP
-    dst->cca[0] = fill_ngap_e2ap_node_component_config_add();
-    // F1AP
-    dst->cca[1] = fill_f1ap_e2ap_node_component_config_add();
-  } else if(n == e2ap_ngran_gNB_DU){
-    dst->len_cca = 1;
-    dst->cca = calloc(1, sizeof(e2ap_node_component_config_add_t));
-    assert(dst->cca != NULL);
-    // F1AP
-    dst->cca[0] = fill_f1ap_e2ap_node_component_config_add();
-  } else if(n == e2ap_ngran_gNB_CUCP){
-    dst->len_cca = 3;
-    dst->cca = calloc(3, sizeof(e2ap_node_component_config_add_t));
-    assert(dst->cca != NULL);
-    // NGAP
-    dst->cca[0] = fill_ngap_e2ap_node_component_config_add();
-    // F1AP
-    dst->cca[1] = fill_f1ap_e2ap_node_component_config_add();
-    // E1AP
-    dst->cca[2] = fill_e1ap_e2ap_node_component_config_add();
-  } else if(n == e2ap_ngran_gNB_CUUP){
-    dst->len_cca = 3;
-    dst->cca = calloc(3, sizeof(e2ap_node_component_config_add_t));
-    assert(dst->cca != NULL);
-    // NGAP
-    dst->cca[0] = fill_ngap_e2ap_node_component_config_add();
-    // F1AP
-    dst->cca[1] = fill_f1ap_e2ap_node_component_config_add();
-    // E1AP
-    dst->cca[2] = fill_e1ap_e2ap_node_component_config_add();
-  } else if(n == e2ap_ngran_eNB){
-    dst->len_cca = 1;
-    dst->cca = calloc(1, sizeof(e2ap_node_component_config_add_t));
-    assert(dst->cca != NULL);
-    // S1AP
-    dst->cca[0] = fill_s1ap_e2ap_node_component_config_add();
-  } else {
-    assert(0 != 0 && "Not implemented");
-  }
+  // Proxy agent amarisoft e2ap_ngran_gNB
+  dst->len_cca = 1;
+  dst->cca = calloc(1, sizeof(e2ap_node_component_config_add_t));
+  assert(dst->cca != NULL);
+  // NGAP
+  dst->cca[0] = fill_ngap_e2ap_node_component_config_add();
 #else
   static_assert(0!=0, "Unknown E2AP version");
 #endif
