b4b728a7fde35d94ca468e731ee94e8aab3c0b48
Chieh-Chun Chen <chenc80348@gmail.com>
2024-03-06 14:58:54 +0100

    Fix: regenerate asn (e42updatee2node) and remove the suffix for E2AP v1

--- src/lib/e2ap/v1_01/ie/asn/ANY.c
+++ src/lib/e2ap/v1_01/ie/asn/ANY.c
@@ -5,29 +5,29 @@
 #include <asn_internal.h>
 #include <ANY.h>
 
-asn_OCTET_STRING_specifics_t asn_SPC_ANY_specs_e2ap_v1_01 = {
+asn_OCTET_STRING_specifics_t asn_SPC_ANY_specs = {
     sizeof(ANY_t),
     offsetof(ANY_t, _asn_ctx),
     ASN_OSUBV_ANY
 };
-asn_TYPE_operation_t asn_OP_ANY_e2ap_v1_01 = {
-    OCTET_STRING_free_e2ap_v1_01,
+asn_TYPE_operation_t asn_OP_ANY = {
+    OCTET_STRING_free,
 #if !defined(ASN_DISABLE_PRINT_SUPPORT)
-    OCTET_STRING_print_e2ap_v1_01,
+    OCTET_STRING_print,
 #else
     0,
 #endif  /* !defined(ASN_DISABLE_PRINT_SUPPORT) */
-    OCTET_STRING_compare_e2ap_v1_01,
+    OCTET_STRING_compare,
 #if !defined(ASN_DISABLE_BER_SUPPORT)
-    OCTET_STRING_decode_ber_e2ap_v1_01,
-    OCTET_STRING_encode_der_e2ap_v1_01,
+    OCTET_STRING_decode_ber,
+    OCTET_STRING_encode_der,
 #else
     0,
     0,
 #endif  /* !defined(ASN_DISABLE_BER_SUPPORT) */
 #if !defined(ASN_DISABLE_XER_SUPPORT)
-    OCTET_STRING_decode_xer_hex_e2ap_v1_01,
-    ANY_encode_xer_e2ap_v1_01,
+    OCTET_STRING_decode_xer_hex,
+    ANY_encode_xer,
 #else
     0,
     0,
@@ -45,15 +45,15 @@ asn_TYPE_operation_t asn_OP_ANY_e2ap_v1_01 = {
     0,
 #endif  /* !defined(ASN_DISABLE_OER_SUPPORT) */
 #if !defined(ASN_DISABLE_UPER_SUPPORT)
-    ANY_decode_uper_e2ap_v1_01,
-    ANY_encode_uper_e2ap_v1_01,
+    ANY_decode_uper,
+    ANY_encode_uper,
 #else
     0,
     0,
 #endif  /* !defined(ASN_DISABLE_UPER_SUPPORT) */
 #if !defined(ASN_DISABLE_APER_SUPPORT)
-    ANY_decode_aper_e2ap_v1_01,
-    ANY_encode_aper_e2ap_v1_01,
+    ANY_decode_aper,
+    ANY_encode_aper,
 #else
     0,
     0,
@@ -61,10 +61,10 @@ asn_TYPE_operation_t asn_OP_ANY_e2ap_v1_01 = {
     0,  /* Random fill is not defined for ANY type */
     0  /* Use generic outmost tag fetcher */
 };
-asn_TYPE_descriptor_t asn_DEF_ANY_e2ap_v1_01 = {
+asn_TYPE_descriptor_t asn_DEF_ANY = {
     "ANY",
     "ANY",
-    &asn_OP_ANY_e2ap_v1_01,
+    &asn_OP_ANY,
     0, 0, 0, 0,
     {
 #if !defined(ASN_DISABLE_OER_SUPPORT)
@@ -73,8 +73,8 @@ asn_TYPE_descriptor_t asn_DEF_ANY_e2ap_v1_01 = {
 #if !defined(ASN_DISABLE_UPER_SUPPORT) || !defined(ASN_DISABLE_APER_SUPPORT)
         0,
 #endif  /* !defined(ASN_DISABLE_UPER_SUPPORT) || !defined(ASN_DISABLE_APER_SUPPORT) */
-        asn_generic_no_constraint_e2ap_v1_01
+        asn_generic_no_constraint
     },  /* No constraints */
     0, 0,  /* No members */
-    &asn_SPC_ANY_specs_e2ap_v1_01,
+    &asn_SPC_ANY_specs,
 };
