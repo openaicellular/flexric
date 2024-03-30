7c57a6fb5bc30fe97cdbb534957866cefa92f343
Teodora <teodora.vladic@openairinterface.org>
2024-03-30 19:01:07 +0100

    Change running time in KPM monitor xApp

--- examples/xApp/c/monitor/xapp_kpm_moni.c
+++ examples/xApp/c/monitor/xapp_kpm_moni.c
@@ -417,7 +417,7 @@ int main(int argc, char* argv[])
   // END KPM
   ////////////
 
-  sleep(10);
+  sleep(1000);
 
   for (int i = 0; i < nodes.len; ++i) {
     // Remove the handle previously returned
