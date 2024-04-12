89a31d4125c1b88809efcadd393f1fd014ad1763
khai nguyen <khai.nguyen@bubbleran.com>
2024-04-12 16:11:00 +0200

    fix ev_trg_ran_conf

--- src/sm/ccc_sm/ie/ir/ev_trg_ran_conf.c
+++ src/sm/ccc_sm/ie/ir/ev_trg_ran_conf.c
@@ -59,7 +59,8 @@ ev_trg_ran_conf_t cp_ev_trg_ran_conf(ev_trg_ran_conf_t const* src)
   if (src->attribute != NULL){
     dst.attribute = calloc(src->sz_attribute, sizeof(attribute_t));
     assert(dst.attribute != NULL && "Memory exhausted");
-    memcpy(dst.attribute, src->attribute, sizeof(attribute_t) * src->sz_attribute);
+    for(size_t i = 0; i < src->sz_attribute; ++i)
+      dst.attribute[i] = cp_attribute(&src->attribute[i]);
   }
 
   return dst;
